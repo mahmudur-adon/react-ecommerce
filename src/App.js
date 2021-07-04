473b7a0d2800a7a4ef463be9514f73a006c9a635
mahmudur-adon <mahmudur.rahmandhk@gmail.com>
2021-07-04 20:21:01 +0600

    shop preview and shop item added to the ui

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './App.css';
 import  HomePage  from './pages/homepage/homepage.component';
 import { Route } from "react-router-dom";
+import ShopPage from './pages/shop/shop.component.jsx'
 
-const Hatspage = () => (
-  <div>
-    <h1>hats available</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={Hatspage} />
+        <Route exact path="/shop" component={ShopPage} />
       </switch>
     </div>
   );
